4378ecc6f922d0221d247e33018f7ae877084604
Charlie Bevis, George Dean and Isobel Redelmeier <pair+cbevis+gdean+iredelmeier@pivotal.io>
2015-08-24 11:14:27 -0700

    React application renders a component

---
+++ js/components/TodoApp.react.js
@@ -0,0 +1,11 @@
+var React = require('react');
+
+var TodoApp = React.createClass({
+    render: function() {
+        return (
+            <div>Hello there</div>
+        )
+    }
+});
+
+module.exports = TodoApp;
